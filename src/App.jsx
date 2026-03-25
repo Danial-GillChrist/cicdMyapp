add48560bbe5d588c6e082d92098469958b29c18
Danial-GillChrist <danialwebdevelopment@gmail.com>
2026-03-25 13:20:48 +0530

    Hide Strict Mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,45 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 // import Greeting  from './utils/add'
 
 function App() {
-
   const [user, helloUser] = useState([]);
 
   const api = "https://dummyjson.com/users";
 
-   async function fetchData () {
-        try{
-            let res = await fetch(api);
-            if(!res.ok){
-               throw new Error("NO data found");
-            }
+  async function fetchData() {
+    try {
+      let res = await fetch(api);
+      if (!res.ok) {
+        throw new Error("NO data found");
+      }
 
-            let data = await res.json();
-            helloUser(data.users);
-            console.log(data.users);
-        }
-        catch(error){
-           console.log(error)
-        }
+      let data = await res.json();
+      helloUser(data.users);
+      // console.log(data.users);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    
-    useEffect(()=>{
-      fetchData()
-    },[]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
 
   return (
     <>
-       <h1>Teting</h1>
-       {
-        user?.map((item)=>{
-          let {firstName, lastName, id} = item;
-          return(
-               <div className='user' key={id}>
-                    <p>{firstName}</p>
-                    <p>{lastName}</p>
-               </div>
-          )
-        })
-       }
+      {user?.map((item) => {
+        let { firstName, lastName, id } = item;
+        return (
+          <div className="user" key={id}>
+            <p>{firstName}</p>
+            <p>{lastName}</p>
+          </div>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
